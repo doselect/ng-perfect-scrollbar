2c54c6e24200517c9d7a80fef16c893a6e2f04a9
Jaipradeesh <jaipradeesh@gmail.com>
2015-10-09 19:13:48 +0530

    Re-generated minified script
    Why?
    gulpfile.js was also being mnified :| Fixed!
    
    Signed-off-by: Jaipradeesh <jai@doselect.com>

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp')
 var uglify = require('gulp-uglifyjs')
 
 gulp.task('uglify', function() {
-  gulp.src('*.js')
-    .pipe(uglify('ng-perfect-scrolbar.min.js'))
+  gulp.src('ng-perfect-scrollbar.js')
+    .pipe(uglify('ng-perfect-scrollbar.min.js'))
     .pipe(gulp.dest('dist'))
 })

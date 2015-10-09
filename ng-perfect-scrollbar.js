729225a2b34fd4e6419f628b97a09495c98d9d7e
Jaipradeesh <jaipradeesh@gmail.com>
2015-10-09 19:13:48 +0530

    Re-generated minified script
    Reason:
    gulpfile.js was also being mnified :| Fixed!
    
    Signed-off-by: Jaipradeesh <jai@doselect.com>

--- ng-perfect-scrollbar.js
+++ ng-perfect-scrollbar.js
@@ -5,23 +5,19 @@ angular.module('ngPerfectScrollbar', [])
   return {
     restrict: 'A',
     scope: { options: '='},
-    link: function($scope, $elem, $attr) {
+    link: function(scope, $elem, $attr) {
       var Window = angular.element($window)
       $elem = angular.element($elem)[0]
 
-      Ps.initialize($elem, $scope.options)
+      Ps.initialize($elem, scope.options)
 
-      if ($attr.refreshOnChange) {
-        $scope.$watchCollection($attr.refreshOnChange, function() {
-          Ps.update($elem)
-        })
-      }
+      Window.on('resize', function() {
+        Ps.update($elem)
+      })
 
-      if ($attr.refreshOnResize) {
-        Window.on('resize', function() {
-          Ps.update($elem)
-        })
-      }
+      scope.$watch($elem.offsetHeight, function() {
+        Ps.update($elem)
+      })
     }
   }
 }])
